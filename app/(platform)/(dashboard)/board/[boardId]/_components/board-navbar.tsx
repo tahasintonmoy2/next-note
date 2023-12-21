12b00de17fae6eab86498a332c016773fe4f3386
Tahasin <110173793+tahasintonmoy2@users.noreply.github.com>
2023-12-21 14:08:19 +0600

    update few components

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,19 +1,26 @@
 import { Board } from "@prisma/client";
 import { BoardTitleForm } from "./board-title-form";
 import { BoardOptions } from "./board-options";
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
 
 interface BoardNavbarProps {
   data: Board;
 }
 
-export const BoardNavbar = async ({ 
-  data 
+export const BoardNavbar = async ({
+  data
 }: BoardNavbarProps) => {
+  const { orgId } = auth()
+
   return (
     <div className="w-full h-14 z-[40] fixed bg-black/50 top-14 flex items-center px-6 gap-x-4 text-white">
+      <Link href={`/organization/${orgId}`} className="text-sm md:block hidden">
+        Return to {data.title} org page.
+      </Link>
       <BoardTitleForm data={data} />
       <div className="ml-auto">
-        <BoardOptions id={data.id} />
+        <BoardOptions id={data.id} board={data} />
       </div>
     </div>
   );
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -8,34 +8,46 @@ import {
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import { useAction } from "@/hooks/use-action";
-import { MoreHorizontal, Trash } from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import { Board } from "@prisma/client";
+import { ArrowLeft, MoreHorizontal, Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 interface BoardOptionsProps {
   id: string;
+  board: Board;
 }
+
 //
-export const BoardOptions = ({ id }: BoardOptionsProps) => {
+export const BoardOptions = ({ id, board }: BoardOptionsProps) => {
   const { execute, isLoading } = useAction(deleteBoard, {
     onError: (error) => {
       toast.error(`Delete fail error is ${error}`);
     },
   });
 
+  const router = useRouter();
+  const { orgId } = useAuth()
+
   const promise = () => new Promise((resolve) => setTimeout(resolve, 2000));
 
   if (isLoading) {
-    toast.promise(promise,{
+    toast.promise(promise, {
       loading: "Please wait few minutes",
-      success:()=> {
+      success: () => {
         return "Board deleted now redirecting to organization page"
-      } 
+      }
     })
   }
 
   const onDelete = () => {
     execute({ id });
   };
+
+  const onRedirect = () => {
+    router.push(`/organization/${orgId}`)
+  }
 
   return (
     <>
@@ -44,6 +56,10 @@ export const BoardOptions = ({ id }: BoardOptionsProps) => {
           <MoreHorizontal className="h-5 w-5" />
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={onRedirect} disabled={isLoading} className="md:hidden items-center flex">
+            <ArrowLeft className="h-5 w-5 mr-2" />
+            Return to {board.title} org page
+          </DropdownMenuItem>
           <DropdownMenuItem onClick={onDelete} disabled={isLoading}>
             <Trash className="h-4 w-4 text-red-600 mr-2" />
             Delete this board
